e58cf1249cccb2c5e9ff274e4149d1fbdcd317e7
joaopedrobagli <joaobagli.pedro1506@gmail.com>
2026-05-21 10:24:28 -0300

    fix: configurar CORS para produção

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,10 +4,11 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:5173',
+    origin: ['http://localhost:5173', 'https://hr-portal-sandy-three.vercel.app'],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true,
   });
   app.setGlobalPrefix('api');
-  await app.listen(3001);
+  await app.listen(process.env.PORT || 3001);
 }
 bootstrap();
